07d47ff4572c63479c2a9a345f9bad6b3e14864f
devaya1 <kanoburu278@gmail.com>
2020-03-03 14:16:20 +0200

    include gulp make gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+
+gulp.task("hello", function (done) {
+    console.log("hello world");
+    done()
+});
+
+
+// Static server
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+
